cc3568e86cfd2b5992658485b9c2f5aa6b9813d7
G4BK4 <vanzxs@mail.ru>
2020-05-12 13:07:11 +0700

    Add in src/ board_print_plain.c and board_print_plain.h

---
+++ src/board_print_plain.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void display_board(char (*board)[10])
+{
+    printf("+---------------------+");
+    for (int i = 0; i < 10; i++) {
+        printf("\n");
+        printf("| ");
+        for (int j = 0; j < 10; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("|");
+    }
+    printf("\n");
+    printf("+---------------------+\n");
+}
+
+// void fprint_board(char (*board)[10])
+// {
+//     FILE* f;
+//     f = fopen("moves.txt", "a");
+
+//     fprintf(f, "+---------------------+");
+//     for (int i = 0; i < 10; i++) {
+//         fprintf(f, "\n");
+//         fprintf(f, "| ");
+//         for (int j = 0; j < 10; j++) {
+//             fprintf(f, "%c ", board[i][j]);
+//         }
+//         fprintf(f, "|");
+//     }
+//     fprintf(f, "\n");
+//     fprintf(f, "+---------------------+\n");
+
+//     fclose(f);
+// }
+
+// void fprint_step(int ffc, int fsc, int sfc, int ssc)
+// {
+//     FILE* f;
+//     f = fopen("moves.txt", "a");
+//     fprintf(f, "%c%d %c%d\n", ffc + 96, 9 - fsc, sfc + 96, 9 - ssc);
+//     fclose(f);
+// }
